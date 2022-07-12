8f7d22378f32248c61da05b44cfc41cae93d87b9
javierbull <javierbullreyes@gmail.com>
2022-07-12 11:38:42 -0500

    sidebar & main panel admin function show hide

--- js/main.js
+++ js/main.js
@@ -21,4 +21,21 @@ $(".sidebar-admin button").click(function(){
 });
 // Admin Dashboard  active item sidebar
 
+// Admin panel
+$("#toggle-btn-sidebar").click(function(){
+     $("#admin-panel").toggleClass("ml-panel ml-panel-0");
+ });
+// End admin panel
+
+// admin btn sidebar togle menu
+const btnToggle = document.querySelector('#toggle-btn-sidebar');
+
+btnToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('sidebar-admin').classList.toggle('active');
+  console.log(document.getElementById('sidebar-admin'))
+});
+
+// End admin btn sidebar togle menu
+
 });
